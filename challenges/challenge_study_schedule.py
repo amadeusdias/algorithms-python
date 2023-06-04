b66bd41babccecdb46f4edf431a6976d1830d505
amadeus <amadeusdias@hotmail.com>
2023-06-04 01:05:48 -0300

    test requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,15 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    counter = 0
+    if target_time is None:
+        return None
+    if not all(isinstance(intervalo,tuple)for intervalo in permanence_period):
+        return None
+    for index, tup in enumerate(permanence_period):
+        if isinstance(tup[0],int) == False:
+            return None
+        if isinstance(tup[1],int) == False: 
+            return None
+        if tup[0] <= target_time <= tup[1]:
+             counter += 1
+        return counter
+    
