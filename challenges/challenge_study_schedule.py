3262eee46ad7cc2bee948d79d7b84dbdec76bf71
amadeus <amadeusdias@hotmail.com>
2023-06-04 01:17:46 -0300

    fix part 3

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -3,7 +3,7 @@ def study_schedule(permanence_period, target_time):
     if target_time is None:
         return None
     for index, tup in enumerate(permanence_period):
-        if isinstance(tup[0], int) is False or if isinstance(tup[1], int) is False:
+        if isinstance(tup[0], int) is False or isinstance(tup[1], int) is False:
             return None
         if tup[0] <= target_time <= tup[1]:
             counter += 1
