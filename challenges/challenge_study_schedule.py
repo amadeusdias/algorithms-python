f1360fbc300d06ad068d980304c3be496838e008
amadeus <amadeusdias@hotmail.com>
2023-06-04 01:12:12 -0300

    fix errors and lint

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,14 +2,11 @@ def study_schedule(permanence_period, target_time):
     counter = 0
     if target_time is None:
         return None
-    if not all(isinstance(intervalo,tuple)for intervalo in permanence_period):
-        return None
     for index, tup in enumerate(permanence_period):
-        if isinstance(tup[0],int) == False:
+        if isinstance(tup[0], int) is False:
             return None
-        if isinstance(tup[1],int) == False: 
+        if isinstance(tup[1], int) is False: 
             return None
         if tup[0] <= target_time <= tup[1]:
              counter += 1
         return counter
-    
