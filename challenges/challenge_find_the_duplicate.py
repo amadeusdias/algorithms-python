3c59f7159deaff6fb5dd339c1004b61321136e5d
amadeus <amadeusdias@hotmail.com>
2023-06-05 19:43:34 -0300

    requisito 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,13 +1,12 @@
-# def find_duplicate(nums):
-#    if not nums:
-#        return False
-#    if len(nums) == 1:
-#        return False
-#    for i, n in enumerate(nums):
-#        if not isinstance(n, int):
-#            return False
-#        elif n < 0:
-#            return False
-#        elif n == nums[i+1]:
-#            return n
-#        else: return False
+def find_duplicate(nums):
+    nums.sort()
+    if not nums:
+        return False
+    if len(nums) == 1:
+       return False
+    for i, n in enumerate(nums):
+        if not isinstance(n, int) or n < 0:
+            return False
+        if n in nums[i + 1:]:
+            return n
+    return False
