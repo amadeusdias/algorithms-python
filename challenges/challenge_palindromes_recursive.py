9853e3776d1f2399f1e2dc1297f136599a4afc5d
amadeus <amadeusdias@hotmail.com>
2023-06-05 17:05:41 -0300

    requisito 3

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,12 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0:
+        return False
+    if len(word) < 2:
+        return True
+    if word[low_index] != word[high_index]:
+        return False
+    if low_index >= high_index:
+        return True
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
+
+# fonte: https://datagy.io/python-palindrome/    
