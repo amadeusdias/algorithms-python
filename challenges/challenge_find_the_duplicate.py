bbbdf8b725c672eebc1a25d2764e212f7303a401
amadeus <amadeusdias@hotmail.com>
2023-06-05 18:25:36 -0300

    req 6

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,13 @@
 def find_duplicate(nums):
-    """Coloque aqui o codigo."""
-    raise NotImplementedError
+    if not nums:
+        return False
+    if len(nums) == 1:
+        return False
+    for i, n in enumerate(nums):
+        if not isinstance(n, int):
+            return False
+        elif n < 0:
+            return False
+        elif n == nums[i+1]:
+            return n
+        else: return False
--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,8 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0:
+        return False
+    for le in range(0, int(len(word)/2)):
+        if word[le] != word[len(word)-le-1]:
+            return False
+    return True
+#font: https://www.geeksforgeeks.org/python-program-check-string-palindrome-not/
