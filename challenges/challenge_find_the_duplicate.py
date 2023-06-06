3571987e9ef3b0d97413de35bb0e9b63b30ea561
amadeus <amadeusdias@hotmail.com>
2023-06-05 21:43:52 -0300

    fix req 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -2,9 +2,11 @@ def find_duplicate(nums):
     nums.sort()
     if not nums or len(nums) == 1:
         return False
+    nlist = set()
     for i, n in enumerate(nums):
         if not isinstance(n, int) or n < 0:
             return False
-        if n in nums[i + 1:]:
+        if n in nlist:
             return n
+        nlist.add(n)
     return False
